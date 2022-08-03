9fc43d295de44848ad81aab75909cec11ccc995e
raghusais <89162131+raghusais@users.noreply.github.com>
2022-08-03 09:41:48 +0530

    Add files via upload

---
+++ WebApp.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+import pickle
+import pandas as pd
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+import plotly.express as px
+
+
+#stemming
+ps = PorterStemmer() 
+
+#loading dataset 
+
+df = pd.read_csv("D:/AI_DeepLearning/Spam_Classifier/data.csv")
+result_df = df.melt('Model', var_name='Metrix', value_name='value')
+
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+        
+    return " ".join(y)
+
+
+ 
+tfidf = pickle.load(open('D:/AI_DeepLearning/Spam_Classifier/vectorizer.pkl','rb'))
+model1 = pickle.load(open('D:/AI_DeepLearning/Spam_Classifier/BNB_model.pkl','rb'))
+model2 = pickle.load(open('D:/AI_DeepLearning/Spam_Classifier/MNB_model.pkl','rb'))
+
+# selecting model from selectbox
+st.sidebar.title('MODEL')
+select_model = st.sidebar.selectbox("Select Model", ("BernoulliNB","MultinomialNB"))
+
+if select_model=="BernoulliNB":
+    model =  model1
+    
+elif select_model=="MultinomialNB":
+    model = model2
+    
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    st.write(transformed_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam Email/SMS")
+    else:
+        st.header("Not Spam/Ham")
+        
+st.write(df) 
+
+
+#sns.catplot(x = 'Model', y='value',hue='Metrix', 
+#               data=result_df, kind='bar',height=5)
+#plt.title("Model Comparison")
+#plt.ylim(0.5,1.2)
+#plt.xticks(rotation='horizontal')
+#plt.show()
+st.title("Accuracy Comparison Plot")
+fig = px.bar(df,x='Model',y='Accuracy',color='Model',range_y=(0.5,1.2))
+st.write(fig)
+
+st.title("Precision Comparison Plot")
+fig1 = px.bar(df,x='Model',y='Precision',color='Model',range_y=(0.3,1.2))
+st.write(fig1)
+
+st.title('Accuracy Vs Precision')
+fig2 = px.line(df,x='Model',y=['Accuracy', 'Precision'],labels={'x':'Model','y':'Value'},range_y=(0.3,1.2))
+st.write(fig2)
